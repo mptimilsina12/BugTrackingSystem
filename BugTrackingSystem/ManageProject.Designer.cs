8e9fa4a84107ead4a781b344c3414325e4b5d541
Mahesh Prasad Timilsina <mptimilsina12@outlook.com>
2018-11-18 22:39:53 +0545

    Coding For Manage Project Completed

--- BugTrackingSystem/ManageProject.Designer.cs
+++ BugTrackingSystem/ManageProject.Designer.cs
@@ -38,19 +38,19 @@
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnDel = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.dateStart = new System.Windows.Forms.DateTimePicker();
-            this.dateEnd = new System.Windows.Forms.DateTimePicker();
+            this.pnlProjectInformation = new System.Windows.Forms.Panel();
             this.txtDescription = new System.Windows.Forms.TextBox();
+            this.dateEnd = new System.Windows.Forms.DateTimePicker();
+            this.dateStart = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dgvProjectInformation = new System.Windows.Forms.DataGridView();
             this.panel4.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.panel1.SuspendLayout();
+            this.pnlProjectInformation.SuspendLayout();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProjectInformation)).BeginInit();
             this.SuspendLayout();
             // 
             // panel4
@@ -134,6 +134,7 @@
             this.btnUpdate.TabIndex = 19;
             this.btnUpdate.Text = "UPDATE";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDel
             // 
@@ -144,6 +145,7 @@
             this.btnDel.TabIndex = 17;
             this.btnDel.Text = "DELETE";
             this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
             // 
             // btnAdd
             // 
@@ -154,80 +156,24 @@
             this.btnAdd.TabIndex = 16;
             this.btnAdd.Text = "ADD";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // panel1
+            // pnlProjectInformation
             // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.txtDescription);
-            this.panel1.Controls.Add(this.dateEnd);
-            this.panel1.Controls.Add(this.dateStart);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.txtPName);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(6, 62);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(729, 175);
-            this.panel1.TabIndex = 8;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // panel3
-            // 
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.dataGridView1);
-            this.panel3.Location = new System.Drawing.Point(6, 296);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(729, 258);
-            this.panel3.TabIndex = 10;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(727, 256);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial Unicode MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(10, 66);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(141, 33);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Start Date:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial Unicode MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(10, 116);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(130, 33);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "End Date:";
-            // 
-            // dateStart
-            // 
-            this.dateStart.Font = new System.Drawing.Font("Microsoft Uighur", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateStart.Location = new System.Drawing.Point(189, 68);
-            this.dateStart.Name = "dateStart";
-            this.dateStart.Size = new System.Drawing.Size(240, 32);
-            this.dateStart.TabIndex = 8;
-            // 
-            // dateEnd
-            // 
-            this.dateEnd.Font = new System.Drawing.Font("Microsoft Uighur", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateEnd.Location = new System.Drawing.Point(189, 118);
-            this.dateEnd.Name = "dateEnd";
-            this.dateEnd.Size = new System.Drawing.Size(240, 32);
-            this.dateEnd.TabIndex = 9;
+            this.pnlProjectInformation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlProjectInformation.Controls.Add(this.txtDescription);
+            this.pnlProjectInformation.Controls.Add(this.dateEnd);
+            this.pnlProjectInformation.Controls.Add(this.dateStart);
+            this.pnlProjectInformation.Controls.Add(this.label5);
+            this.pnlProjectInformation.Controls.Add(this.label4);
+            this.pnlProjectInformation.Controls.Add(this.label2);
+            this.pnlProjectInformation.Controls.Add(this.txtPName);
+            this.pnlProjectInformation.Controls.Add(this.label3);
+            this.pnlProjectInformation.Location = new System.Drawing.Point(6, 62);
+            this.pnlProjectInformation.Name = "pnlProjectInformation";
+            this.pnlProjectInformation.Size = new System.Drawing.Size(729, 175);
+            this.pnlProjectInformation.TabIndex = 8;
+            this.pnlProjectInformation.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // txtDescription
             // 
@@ -239,6 +185,65 @@
             this.txtDescription.Size = new System.Drawing.Size(269, 107);
             this.txtDescription.TabIndex = 10;
             // 
+            // dateEnd
+            // 
+            this.dateEnd.Font = new System.Drawing.Font("Microsoft Uighur", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateEnd.Location = new System.Drawing.Point(189, 118);
+            this.dateEnd.Name = "dateEnd";
+            this.dateEnd.Size = new System.Drawing.Size(240, 32);
+            this.dateEnd.TabIndex = 9;
+            // 
+            // dateStart
+            // 
+            this.dateStart.Font = new System.Drawing.Font("Microsoft Uighur", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateStart.Location = new System.Drawing.Point(189, 68);
+            this.dateStart.Name = "dateStart";
+            this.dateStart.Size = new System.Drawing.Size(240, 32);
+            this.dateStart.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Unicode MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(10, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 33);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "End Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Unicode MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 33);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Start Date:";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.dgvProjectInformation);
+            this.panel3.Location = new System.Drawing.Point(6, 296);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(729, 258);
+            this.panel3.TabIndex = 10;
+            // 
+            // dgvProjectInformation
+            // 
+            this.dgvProjectInformation.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProjectInformation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProjectInformation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProjectInformation.Location = new System.Drawing.Point(0, 0);
+            this.dgvProjectInformation.Name = "dgvProjectInformation";
+            this.dgvProjectInformation.RowTemplate.Height = 24;
+            this.dgvProjectInformation.Size = new System.Drawing.Size(727, 256);
+            this.dgvProjectInformation.TabIndex = 0;
+            this.dgvProjectInformation.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProjectInformation_CellClick);
+            // 
             // ManageProject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -246,20 +251,21 @@
             this.ClientSize = new System.Drawing.Size(743, 559);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlProjectInformation);
             this.Controls.Add(this.panel3);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ManageProject";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ManageProject";
+            this.Load += new System.EventHandler(this.ManageProject_Load);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.panel2.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.pnlProjectInformation.ResumeLayout(false);
+            this.pnlProjectInformation.PerformLayout();
             this.panel3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProjectInformation)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -276,9 +282,9 @@
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnDel;
         private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlProjectInformation;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvProjectInformation;
         private System.Windows.Forms.DateTimePicker dateEnd;
         private System.Windows.Forms.DateTimePicker dateStart;
         private System.Windows.Forms.Label label5;
--- BugTrackingSystem/ManageProject.cs
+++ BugTrackingSystem/ManageProject.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using DataAccessLayer;
+using BusinessLogicLayer;
 namespace BugTrackingSystem
 {
     public partial class ManageProject : Form
@@ -16,7 +17,10 @@ namespace BugTrackingSystem
         {
             InitializeComponent();
         }
-
+        ProjectClass projectclass = new ProjectClass();
+        BusinessLogicClass businesslogicclass = new BusinessLogicClass();
+       public int ProjectId;
+     
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
@@ -25,6 +29,102 @@ namespace BugTrackingSystem
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void ManageProject_Load(object sender, EventArgs e)
+        {
+            dgvProjectInformation.DataSource = projectclass.getAllProjects();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool result = businesslogicclass.manageProjects(0, txtPName.Text, Convert.ToDateTime(dateStart.Text), Convert.ToDateTime(dateEnd.Text), txtDescription.Text, 1);
+                if (result == true)
+                {
+                    MessageBox.Show("NEW PROJECT HAS BEEN SUCCESSFULLY ADDED");
+                    dgvProjectInformation.DataSource = projectclass.getAllProjects();
+                    AssistantClass.makeFieldsBlank(pnlProjectInformation);
+                }
+                else
+                {
+                    MessageBox.Show("ERROR ON ADDING NEW PROJECT");
+                    dgvProjectInformation.DataSource = projectclass.getAllProjects();
+                    AssistantClass.makeFieldsBlank(pnlProjectInformation);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void dgvProjectInformation_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                ProjectId = Convert.ToInt32(dgvProjectInformation.SelectedRows[0].Cells["projectId"].Value.ToString());
+                txtPName.Text = dgvProjectInformation.SelectedRows[0].Cells["projectName"].Value.ToString();
+                dateStart.Text = dgvProjectInformation.SelectedRows[0].Cells["projectStartDate"].Value.ToString();
+                dateEnd.Text = dgvProjectInformation.SelectedRows[0].Cells["projectEndDate"].Value.ToString();
+                txtDescription.Text = dgvProjectInformation.SelectedRows[0].Cells["projectDescription"].Value.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool result = businesslogicclass.manageProjects(ProjectId, txtPName.Text, Convert.ToDateTime(dateStart.Text), Convert.ToDateTime(dateEnd.Text), txtDescription.Text, 2);
+                if (result == true)
+                {
+                    MessageBox.Show(" PROJECT HAS BEEN SUCCESSFULLY UPDATED");
+                    dgvProjectInformation.DataSource = projectclass.getAllProjects();
+                    AssistantClass.makeFieldsBlank(pnlProjectInformation);
+                }
+                else
+                {
+                    MessageBox.Show("ERROR ON UPDATING EXISTING PROJECT");
+                    dgvProjectInformation.DataSource = projectclass.getAllProjects();
+                    AssistantClass.makeFieldsBlank(pnlProjectInformation);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool result = businesslogicclass.manageProjects(ProjectId, txtPName.Text, Convert.ToDateTime(dateStart.Text), Convert.ToDateTime(dateEnd.Text), txtDescription.Text, 3);
+                if (result == true)
+                {
+                    MessageBox.Show(" PROJECT INFORMATION HAS BEEN SUCCESSFULLY DELETED");
+                    dgvProjectInformation.DataSource = projectclass.getAllProjects();
+                    AssistantClass.makeFieldsBlank(pnlProjectInformation);
+                }
+                else
+                {
+                    MessageBox.Show("ERROR ON DELETING EXISTING PROJECT");
+                    dgvProjectInformation.DataSource = projectclass.getAllProjects();
+                    AssistantClass.makeFieldsBlank(pnlProjectInformation);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
